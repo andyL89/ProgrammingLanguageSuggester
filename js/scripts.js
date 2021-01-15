d1805859e45493dbfb5032b2d7aaf1d870919e92
Andy Lawson <alawson89@gmail.com>
2021-01-15 11:05:48 -0800

    fix broken js experiment

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,23 @@
 $(document).ready(function() {
-  $(".btn1").submit1(function(event) {
-    $("#formOne").show();
-    event.preventDefault();
-  // });
-  $("#formTwo").submit2(function(event) {
-    const ageInput = parseInt($("input#age").val());
-    const nameInput = $("input#name").val();
-    const operatingSystemInput = $("input#operatingSystem").val();
-    const whichEndInput = $("input#whichEnd").val();
-    const experienceInput = $("input#experience").val();
-    const troubleShootInput = $("input#troubleShoot").val();
-
-    $(".age").text(ageInput);
-    $(".name").text(nameInput);
-    $(".operatingSystem").text(operatingSystemInput);
-    $(".whichEnd").text(whichEndInput);
-    $(".experience").text(experienceInput);
-    $(".troubleShoot").text(troubleShootInput);
-
-    $("#confirmation").show();
-
-    event.preventDefault();
-  });
+  
+    
+    $("#formOne").submit(function(event) {
+      const nameInput = $("input#name").val();
+      const operatingSystemInput = $("input#operatingSystem").val();
+      const whichEndInput = $("input#whichEnd").val();
+      const experienceInput = $("input#experience").val();
+      const troubleShootInput = $("input#troubleShoot").val();
+      
+      $(".name").text(nameInput);
+      $(".operatingSystem").text(operatingSystemInput);
+      $(".whichEnd").text(whichEndInput);
+      $(".experience").text(experienceInput);
+      $(".troubleShoot").text(troubleShootInput);
+      
+      $("#confirmation").show();
+      
+      event.preventDefault();
+      
+    
   });
 });
