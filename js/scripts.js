e36bdfd1fa49d58c90180699cef5410137a84b59
Andy Lawson <alawson89@gmail.com>
2021-01-15 11:39:38 -0800

    add box element for #confirmation

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,22 @@
-$(document).ready(function() {
-  
-    
-    $("#formOne").submit(function(event) {
-      const nameInput = $("input#name").val();
-      const operatingSystemInput = $("input#operatingSystem").val();
-      const whichEndInput = $("input#whichEnd").val();
-      const experienceInput = $("input#experience").val();
-      const troubleShootInput = $("input#troubleShoot").val();
-      
-      $(".name").text(nameInput);
-      $(".operatingSystem").text(operatingSystemInput);
-      $(".whichEnd").text(whichEndInput);
-      $(".experience").text(experienceInput);
-      $(".troubleShoot").text(troubleShootInput);
-      
-      $("#confirmation").show();
-      
-      event.preventDefault();
-      
-    
+$(document).ready(function () {
+  $("#formOne").submit(function (event) {
+    const nameInput = $("input#name").val();
+    const operatingSystemInput = $("input#operatingSystem").val();
+    const whichEndInput = $("input#whichEnd").val();
+    const experienceInput = $("input#experience").val();
+    const puzzleInput = $("input#puzzle").val();
+
+    $(".name").text(nameInput);
+    $(".operatingSystem").text(operatingSystemInput);
+    $(".whichEnd").text(whichEndInput);
+    $(".experience").text(experienceInput);
+    $(".puzzle").text(puzzleInput);
+
+    $("#confirmation").show();
+
+    event.preventDefault();
+
+
   });
 });
+
